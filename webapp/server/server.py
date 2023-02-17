de75daf6e3fd428908a4c3279910a205a5c971ad
Jacob Taegon Kim <jacobkim@berkeley.edu>
2023-02-17 06:23:02 -0800

    procfile and server

--- webapp/server/server.py
+++ webapp/server/server.py
@@ -111,6 +111,6 @@ def delete(name):
 
 
 if __name__ == '__main__':
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0')
