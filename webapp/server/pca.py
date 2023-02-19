745cd5b860f3237470aa380792b052185336d30f
Jacob Taegon Kim <jacobkim@berkeley.edu>
2023-02-19 00:10:59 -0800

    change re [0]

--- webapp/server/pca.py
+++ webapp/server/pca.py
@@ -132,7 +132,7 @@ def fitting():
 def checkformat(file):
     logger.info("Check format ----- line 1------")
     logger.info(file)
-    filename = re.findall(r"[^/]+(?=/$|$)", file)
+    filename = re.findall(r"[^/]+(?=/$|$)", file)[0]
     logger.info("File name :")
     logger.info(filename)
     logger.info("Current directory:")
