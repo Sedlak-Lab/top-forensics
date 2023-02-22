e0764c547866698de8ab89dfae07ca6ccd0db28e
Jacob Taegon Kim <jacobkim@berkeley.edu>
2023-02-22 06:09:43 -0800

    added about

--- webapp/static/jsx/App.jsx
+++ webapp/static/jsx/App.jsx
@@ -7,10 +7,12 @@ import Navbar from 'react-bootstrap/Navbar'
 import Notebook from './Components/Notebook/Notebook'
 import axios from 'axios'
 import InitialBook from './Components/Notebook/Initialbook';
+import Alert from 'react-bootstrap/Alert';
 
 function App() {
 
   const [files, setFiles] = useState([])
+  const [showAbout, setshowAbout] = useState(false)
   console.log(files)
   
   const removeFile = (filename) => {
@@ -25,7 +27,7 @@ function App() {
         <Navbar.Brand href='#home'>PFAS Forensics Using the TOP Assay</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#home">About</Nav.Link>
+          <Nav.Link href="#home" onClick={() => setshowAbout(true)}>About</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
@@ -33,9 +35,19 @@ function App() {
       <div className="App">
           <FileUpload files={files} setFiles = {setFiles} removeFile={removeFile} />
         </div>
-        <div className='Notebook'>
+        {
+                        showAbout ?
+                              <Alert variant="light" onClose={() => setshowAbout(false)} dismissible bsPrefix='alert'>
+                                <Alert.Heading>About</Alert.Heading>
+                                <p>
+                                This research was funded by the Strategic Environmental Research and Development Program (SERDP ER-1330). 
+                                If you have questions or would like to contribute TOP data, please email David Sedlak at sedlak@berkeley.edu.
+                                </p>
+                              </Alert> :
+                              <div className='Notebook'>
           {Array.isArray(files) && files.length ? <Notebook notebook={files} /> : <InitialBook />}
         </div>
+          }
     </div>
     </div>
   );
