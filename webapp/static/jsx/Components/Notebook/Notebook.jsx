3c44e87b8288b9666e76aa0d9bbb8148a24f2ade
Jacob Taegon Kim <jacobkim@berkeley.edu>
2023-03-02 04:02:54 -0800

    button not display until loaded

--- webapp/static/jsx/Components/Notebook/Notebook.jsx
+++ webapp/static/jsx/Components/Notebook/Notebook.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Notebook.scss'
 
 const Notebook = ({notebook}) => {
   const filename = notebook[0].name
   console.log(filename)
+  const [load, setLoad] = useState(false)
 
 
   return (
     <div className='render'>
       <div className='notebook'>
-        <img src={`https://ned-web-app.herokuapp.com/render/${filename}?${Date.now()}`} alt="Notebook" />
-        {!notebook[0].isUploading &&
+        <img src={`https://ned-web-app.herokuapp.com/render/${filename}?${Date.now()}`} alt="Notebook" onLoad={() => setLoad(true)}/>
+        {!notebook[0].isUploading && load === true ?
                 <button>
                 <a href={`https://ned-web-app.herokuapp.com/downloads/${filename}`}>Download</a>
-              </button>
+              </button> : <div></div>
               }
       </div>
     </div>
